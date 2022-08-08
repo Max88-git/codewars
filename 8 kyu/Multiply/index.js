4627d5e967b0796ad027da89897e9176766c134f
Max Lockwood <mlockwood@hotmail.co.uk>
2022-08-08 19:17:53 +0100

    Add new kata

---
+++ 8 kyu/Multiply/index.js
@@ -0,0 +1,23 @@
+/*
+  Title:
+    Multiply
+  Description:
+    This code does not execute properly. Try to figure out why.
+  Kata Link:
+    https://www.codewars.com/kata/multiply
+  Discuss Link:
+    https://www.codewars.com/kata/multpily/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/multiply/solutions
+*/
+
+// Solution
+function multiply(a, b) {
+  answer = a * b;
+  return answer;
+}
+
+//Best Practices
+multiply = function (a, b) {
+  return a * b;
+};
