2ec40218398bb027602b38a7c188f09946f259f7
Max Lockwood <mlockwood@hotmail.co.uk>
2022-10-12 21:00:02 +0100

    Add new kata

---
+++ 8 kyu/Function 2 - squaring an argument/index.js
@@ -0,0 +1,20 @@
+/*
+  Title:
+    Function 2 - squaring an argument
+  Description:
+    Now you have to write a function called square that takes an argument and returns the square of it.
+  Kata Link:
+    https://www.codewars.com/kata/function-2-squaring-an-argument
+  Discuss Link:
+    https://www.codewars.com/kata/function-2-squaring-an-argument/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/function-2-squaring-an-argument/solutions
+*/
+
+// Solution
+function square(no) {
+  return no ** 2;
+}
+
+//Best Practices
+const square = (n) => n * n;
