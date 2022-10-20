636258b585e0deb8729c4dd8dec18a5a42747f8f
Max Lockwood <mlockwood@hotmail.co.uk>
2022-10-20 19:12:40 +0100

    Add new kata

---
+++ 8 kyu/MakeUpperCase/index.js
@@ -0,0 +1,20 @@
+/*
+  Title:
+    MakeUpperCase
+  Description:
+    Write a function which converts the input string to uppercase.
+  Kata Link:
+    https://www.codewars.com/kata/makeuppercase
+  Discuss Link:
+    https://www.codewars.com/kata/makeuppercase/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/makeuppercase/solutions
+*/
+
+// Solution
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+// Best Practices
+const makeUpperCase = (str) => str.toUpperCase();
