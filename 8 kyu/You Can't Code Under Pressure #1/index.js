1ef6e8ee2d1c4f0675d0a57593e61b4c55904a10
Max Lockwood <mlockwood@hotmail.co.uk>
2022-10-21 19:44:32 +0100

    Add new kata

---
+++ 8 kyu/You Can't Code Under Pressure #1/index.js
@@ -0,0 +1,20 @@
+/*
+  Title:
+    You Can't Code Under Pressure #1
+  Description:
+    Code as fast as you can! You need to double the integer and return it.
+  Kata Link:
+    https://www.codewars.com/kata/you-cant-code-under-pressure-number-1
+  Discuss Link:
+    https://www.codewars.com/kata/you-cant-code-under-pressure-number-1/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/you-cant-code-under-pressure-number-1/solutions
+*/
+
+// Solution
+function doubleInteger(i) {
+  return i * 2;
+}
+
+// Best Practices
+const doubleInteger = (i) => 2 * i;
