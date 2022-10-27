979422d4e541ff024947f0f0810e20d54593ca2f
Max Lockwood <mlockwood@hotmail.co.uk>
2022-10-27 19:24:24 +0100

    Add new kata

---
+++ 8 kyu/You only need one - Beginner/index.js
@@ -0,0 +1,22 @@
+/*
+  Title:
+    You only need one - Beginner
+  Description:
+    You will be given an array (a) and a value (x).
+    All you need to do is check whether the provided array contains the value.
+    Array can contain numbers or strings. X can be either.
+    Return true if the array contains the value, false if not.
+  Kata Link:
+    https://www.codewars.com/kata/you-only-need-one-beginner
+  Discuss Link:
+    https://www.codewars.com/kata/you-only-need-one-beginner/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/you-only-need-one-beginner/solutions
+*/
+
+// Solution
+function check(a, x) {
+  return a.includes(x);
+}
+// Best Practices
+const check = (a, x) => a.includes(x);
