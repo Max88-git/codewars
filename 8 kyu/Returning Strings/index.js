5b31c44c18f0f99b922ce3c9207250e70da384c2
Max Lockwood <mlockwood@hotmail.co.uk>
2022-11-20 15:23:32 +0000

    Add new kata

---
+++ 8 kyu/Returning Strings/index.js
@@ -0,0 +1,22 @@
+/*
+  Title:
+    Returning Strings
+  Description:
+    Make a function that will return a greeting statement that uses an input;
+    your program should return, "Hello, <name> how are you doing today?".
+    [Make sure you type the exact thing I wrote or the program may not execute properly]
+  Kata Link:
+    https://www.codewars.com/kata/returning-strings
+  Discuss Link:
+    https://www.codewars.com/kata/returning-strings/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/returning-strings/solutions
+*/
+
+// Solution
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+// Best Practices
+// Same solution as mine.
