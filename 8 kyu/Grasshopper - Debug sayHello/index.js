fb40b5050cf2f0921952b6d04673822ce1cc9cb2
Max Lockwood <mlockwood@hotmail.co.uk>
2022-12-31 13:28:05 +0000

    Add new kata

---
+++ 8 kyu/Grasshopper - Debug sayHello/index.js
@@ -0,0 +1,23 @@
+/*
+  Title:
+    Grasshopper - Debug sayHello
+  Description:
+    The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard.
+    It is your job to fix the code and get the program working again!
+  Examples:
+    Hello, Mr. Spock
+  Kata Link:
+    https://www.codewars.com/kata/grasshopper-debug-sayhello
+  Discuss Link:
+    https://www.codewars.com/kata/grasshopper-debug-sayhello/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/grasshopper-debug-sayhello/solutions
+*/
+
+// Solution
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
+
+//Best Practices
+const sayHello = (name) => `Hello, ${name}`;
